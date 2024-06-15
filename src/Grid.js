bfb08014af4ebbe7945c52d32b84ba9ed571aab3
uhmdkhms <uhmdkhms@gmail.com>
2024-06-15 10:59:33 +0300

    imported svgs

--- src/Grid.js
+++ src/Grid.js
@@ -1,5 +1,36 @@
 import { Fragment } from "react";
 
+import apple from "./img/apple.svg";
+import appleGreen from "./img/apple-green.svg";
+import cat from "./img/cat.svg";
+import catGrin from "./img/catGrin.svg";
+import cloud from "./img/cloud.svg";
+import cloudLightning from "./img/cloudLightning.svg";
+import cloudLightningRain from "./img/cloudLightningRain.svg";
+import cloudRain from "./img/cloudRain.svg";
+import cloudSnow from "./img/cloudSnow.svg";
+import cloudSun from "./img/cloudSun.svg";
+import faceDisguised from "./img/faceDisguised.svg";
+import faceFrown from "./img/faceFrown.svg";
+import faceMonocle from "./img/faceMonocle.svg";
+import faceNoMouth from "./img/faceNoMouth.svg";
+import faceOpenMouth from "./img/faceOpenMouth.svg";
+import faceRaisedEyebrow from "./img/faceRaisedEyebrow.svg";
+import faceSlightlySmiling from "./img/faceSlightlySmiling.svg";
+import fire from "./img/fire.svg";
+import ice from "./img/ice.svg";
+import moon from "./img/moon.svg";
+import orange from "./img/orange.svg";
+import palmTree from "./img/palmTree.svg";
+import pineapple from "./img/pineapple.svg";
+import questionMark from "./img/questionMark.svg";
+import star from "./img/star.svg";
+import starGlowing from "./img/starGlowing.svg";
+import sun from "./img/sun.svg";
+import tree from "./img/tree.svg";
+import water from "./img/water.svg";
+import watermelon from "./img/watermelon.svg";
+
 export default function Grid() {
   let matrix = Array(6)
     .fill()
@@ -14,7 +45,7 @@ export default function Grid() {
   function generatePuzzle(matrix) {
     const width = matrix[0].length;
     const height = matrix.length;
-    const n = rand(10);
+    const n = rand(30);
 
     const cell1 = { x: rand(width), y: rand(height) };
     const cell2 = { x: rand(width), y: rand(height) };
@@ -36,7 +67,7 @@ export default function Grid() {
     if ((cell1.x === cell2.x && cell1.y === cell2.y) || isCellOccupied)
       generatePuzzle(matrix);
     else {
-      matrix[cell1.y][cell1.x] = <p>{n}</p>;
+      matrix[cell1.y][cell1.x] = <div>{}</div>;
       matrix[cell2.y][cell2.x] = <p>{n}</p>;
       occupiedCells.push(cell1);
       occupiedCells.push(cell2);
